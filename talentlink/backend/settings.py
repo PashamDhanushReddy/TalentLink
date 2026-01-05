8e32a89b7cc9f9e867df56e565c2963cf0717302
PashamDhanushReddy <pashamdhanushreddy@gmail.com>
2026-01-05 22:24:02 +0530

    Fix Django BigAutoField configuration error

--- talentlink/backend/settings.py
+++ talentlink/backend/settings.py
@@ -154,7 +154,7 @@ STATICFILES_DIRS = [
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Django REST Framework
 REST_FRAMEWORK = {
