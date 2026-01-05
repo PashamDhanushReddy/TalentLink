87f5d1ab520d88d45ac2b2298c272cb5fea914bb
PashamDhanushReddy <pashamdhanushreddy@gmail.com>
2026-01-05 09:51:25 +0530

    done1

--- talentlink/build.sh
+++ talentlink/build.sh
@@ -1,8 +1,5 @@
-#!/usr/bin/env bash
-# exit on error
 set -o errexit
-
 pip install -r requirements.txt
-
+cd talentlink || exit 1
 python manage.py collectstatic --no-input
 python manage.py migrate
