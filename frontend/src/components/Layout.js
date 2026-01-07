8b5d99a636b5b82a82663a5b48b3b89f251371d5
PashamDhanushReddy <pashamdhanushreddy@gmail.com>
2026-01-07 11:02:33 +0530

    chat fix5

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -27,14 +27,12 @@ const Layout = () => {
         </div>
       </div>
       
-      <div className="flex-1 flex flex-col overflow-hidden">
+      <div className="flex-1 flex flex-col">
         <div className="sticky top-0 z-40">
           <TopBar onMenuClick={() => setSidebarOpen(!sidebarOpen)} />
         </div>
-        <main className={`flex-1 bg-gray-50 ${
-          isChatsPage
-            ? 'overflow-hidden p-0'
-            : 'overflow-x-hidden overflow-y-auto p-4 md:p-6 lg:p-8'
+        <main className={`flex-1 overflow-x-hidden overflow-y-auto bg-gray-50 ${
+          isChatsPage ? 'p-0' : 'p-4 md:p-6 lg:p-8'
         }`}>
           <div className="w-full">
             <Outlet />
